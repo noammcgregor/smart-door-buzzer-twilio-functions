19fb79291ad11992046f62aab11fe2d7fd228612
noammcgregor <nmcgregor96@gmail.com>
2024-08-21 22:27:21 -0700

    Update call-residents.js

--- call-residents.js
+++ call-residents.js
@@ -1,38 +1,11 @@
 exports.handler = function(context, event, callback) {
-    let twiml = new Twilio.twiml.VoiceResponse();
-    
-    // Get rid of random non-alphabetical chars and put to lower case
-    if (event.SpeechResult !== undefined) {
-        var cleanString = event.SpeechResult.replace(/[^\w\s]|_/g, "")
-            .replace(/\s+/g, " ");
-        var cleanSpeechResult = cleanString.toLowerCase(); 
-    } else {
-        var cleanSpeechResult = " ";
-    }
+  let twiml = new Twilio.twiml.VoiceResponse();
   
-    console.log('Speech: ' + cleanSpeechResult + '; confidence: ' + event.Confidence)
-    console.log('Digits: ' + event.Digits)
-    
-    if (cleanSpeechResult == context.PASSPHRASE && event.Confidence > 0.5) {
-        // Check if we have a passphrase match, and open the door
-        twiml.say({voice: 'man'}, 'Welcome! friend!')
-        twiml.play({digits: '6666'}) // Pressing 6 sends DTFM tone to open the door
-        twiml.pause({length:1})
-        
-        // Also send me a text on this 
-        twiml.redirect('/text-me?Method=phrase')
-        callback(null, twiml)  
-    } else if (event.Digits == context.PASSCODE){
-        // Check if we have a passcode match, and open the door
-        twiml.say({voice: 'man'}, 'Welcome! friend!')
-        twiml.play({digits: '6666'}) // Pressing 6 sends DTFM tone to open the door
-        twiml.pause({length:1})
-        
-        // Also send me a text on this 
-        twiml.redirect('/text-me?Method=code')
-        callback(null, twiml)
-    }else {
-        twiml.redirect('/call-residents')
-        callback(null, twiml)
-    }
+  // If no valid answer after timeout, dial all residents until someone picks up
+  var path = '/text-me?Method=call'
+  let dial = twiml.dial({action: path});
+  dial.number(context.NOAM_PHONE);
+  dial.number(context.MANDA_PHONE);
+  
+  callback(null, twiml)  
 }
